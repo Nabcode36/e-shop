1113ca3021b2624b39916165e0b1172901ff5946
Nabeel Pathan <z5359531@ad.unsw.edu.au>
2025-01-23 15:25:32 +1100

    Created .env to store port number

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000; // 5000 is the default port
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+app.listen(PORT, () => {
+    console.log("Server is running on http://localhost:" + PORT);
 });
