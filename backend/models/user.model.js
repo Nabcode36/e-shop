23ad7caa6035d8cd41e1a7f9266b5a014127a247
Nabeel Pathan <z5359531@ad.unsw.edu.au>
2025-01-24 16:50:42 +1100

    Setup the user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -58,7 +58,7 @@ userSchema.pre("save", async function (next) {
 })
 
 userSchema.methods.comparePassword = async function (password) {
-  
+  return bcrypt.compare(password, this.password);
 }
 
 export default User;
