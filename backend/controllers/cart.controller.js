9528293508860200020724471a2e023d1d27c239
Nabeel Pathan <z5359531@ad.unsw.edu.au>
2025-02-03 09:14:46 +1100

    Finished the coupon routes

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -6,7 +6,7 @@ export const getCartProducts = async (req, res) => {
 
     // add quanittiy for each product
     const cartItems = products.map(product => {
-      const item = req.user.cartItems.find(cartItem => cartItem.id === product._id);
+      const item = req.user.cartItems.find(cartItem => cartItem.id === product.id);
       return { ...product.toJSON(), quantity: item.quantity }
     });
 
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -16,8 +16,8 @@ router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
 router.get("/category/:category", getProductsByCategory);
 router.get("/recommendations", getRecommendedProducts);
-router.post("/create", protectRoute, adminRoute, createProduct);
-router.patch("/patch/:id", protectRoute, adminRoute, toggleFeaturedProduct);
-router.delete("/delete/:id", protectRoute, adminRoute, deleteProduct);
+router.post("/", protectRoute, adminRoute, createProduct);
+router.patch("/:id", protectRoute, adminRoute, toggleFeaturedProduct);
+router.delete("/:id", protectRoute, adminRoute, deleteProduct);
 
 export default router;
