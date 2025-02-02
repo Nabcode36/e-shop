a8f4be8a55c6018eae693bfbfacb31836f61386d
Nabeel Pathan <z5359531@ad.unsw.edu.au>
2025-02-03 08:39:43 +1100

    Finished the cart routes

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { addToCart } from "../controllers/cart.controller.js";
+import { addToCart, getCartProducts, removeAllFromCart, updateQuantity } from "../controllers/cart.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/", addToCart);
+router.get("/", protectRoute, getCartProducts);
+router.post("/", protectRoute, addToCart);
+router.delete("/", protectRoute, removeAllFromCart);
+router.put("/:id", protectRoute, updateQuantity);
+
 
 export default router;
