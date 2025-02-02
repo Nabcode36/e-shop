f433beb44982d2fbc849b2fd07f2d4784f89ed8f
Nabeel Pathan <z5359531@ad.unsw.edu.au>
2025-02-03 08:17:41 +1100

    Finished auth and product routes and started the cart routes

---
+++ backend/routes/product.route.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { 
+  createProduct,
+  deleteProduct,
+  getAllProducts, 
+  getProductsByCategory, 
+  getRecommendedProducts, 
+  getFeaturedProducts,
+  toggleFeaturedProduct
+} from "../controllers/product.controller.js";
+import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
+router.get("/category/:category", getProductsByCategory);
+router.get("/recommendations", getRecommendedProducts);
+router.post("/create", protectRoute, adminRoute, createProduct);
+router.patch("/patch/:id", protectRoute, adminRoute, toggleFeaturedProduct);
+router.delete("/delete/:id", protectRoute, adminRoute, deleteProduct);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 // routes
-import authRoutes from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -11,8 +15,11 @@ const app = express();
 const PORT = process.env.PORT || 5000; // 5000 is the default port
 
 app.use(express.json()); // allows for parsing the body of the request
+app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
