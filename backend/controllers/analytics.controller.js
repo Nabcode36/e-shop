5c00ebc65bd9af0d4b872d73f5494538de9c1e8b
Nabeel Pathan <z5359531@ad.unsw.edu.au>
2025-02-03 19:06:22 +1100

    Finished the analytics routes

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -2,6 +2,7 @@ import User from "../models/user.model.js";
 import Product from "../models/product.model.js";
 import Order from "../models/order.model.js";
 
+
 export const getAnalyticsData = async (req, res) => {
   const totalUsers = await User.countDocuments();
   const totalProducts = await Product.countDocuments();
@@ -23,5 +24,64 @@ export const getAnalyticsData = async (req, res) => {
     products: totalProducts,
     totalSales,
     totalRevenue
-  }
+  };
 };
+
+export const getDailySalesData = async (startDate, endDate) => {
+	try {
+		const dailySalesData = await Order.aggregate([
+			{
+				$match: {
+					createdAt: {
+						$gte: startDate,
+						$lte: endDate,
+					},
+				},
+			},
+			{
+				$group: {
+					_id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+					sales: { $sum: 1 },
+					revenue: { $sum: "$totalAmount" },
+				},
+			},
+			{ $sort: { _id: 1 } },
+		]);
+
+		// example of dailySalesData
+		// [
+		// 	{
+		// 		_id: "2024-08-18",
+		// 		sales: 12,
+		// 		revenue: 1450.75
+		// 	},
+		// ]
+
+		const dateArray = getDatesInRange(startDate, endDate);
+		// console.log(dateArray) // ['2024-08-18', '2024-08-19', ... ]
+
+		return dateArray.map((date) => {
+			const foundData = dailySalesData.find((item) => item._id === date);
+
+			return {
+				date,
+				sales: foundData?.sales || 0,
+				revenue: foundData?.revenue || 0,
+			};
+		});
+	} catch (error) {
+		throw error;
+	}
+};
+
+function getDatesInRange(startDate, endDate) {
+	const dates = [];
+	let currentDate = new Date(startDate);
+
+	while (currentDate <= endDate) {
+		dates.push(currentDate.toISOString().split("T")[0]);
+		currentDate.setDate(currentDate.getDate() + 1);
+	}
+
+	return dates;
+}
