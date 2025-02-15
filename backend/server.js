7520b78dde8e0f100aeb993f91cf60659c301eb0
Nabeel Pathan <z5359531@ad.unsw.edu.au>
2025-02-15 19:15:09 +1100

    Created the layout for the admin dashboard and product creation

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000; // 5000 is the default port
 
-app.use(express.json()); // allows for parsing the body of the request
+app.use(express.json({ limit: "10mb" })); // allows for parsing the body of the request
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
